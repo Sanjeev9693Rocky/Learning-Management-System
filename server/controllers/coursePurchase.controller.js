4b29d48792d62334331ad8bf3d47e7ab3b1c0537
ranjan <ranjansanjeev2005@email.com>
2025-08-15 16:42:18 +0530

    LMS

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -5,8 +5,6 @@ import { User } from "../models/user.model.js";
 import Razorpay from 'razorpay'
 import crypto from 'node:crypto'
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 export const createCheckoutSession = async (req, res) => {
   try {
     const userId = req.id;
@@ -48,49 +46,6 @@ export const createCheckoutSession = async (req, res) => {
     res.status(500).send("Error");
   }
 
-  // Create a Stripe checkout session
-  // const session = await stripe.checkout.sessions.create({
-  //   payment_method_types: ["card"],
-  //   line_items: [
-  //     {
-  //       price_data: {
-  //         currency: "inr",
-  //         product_data: {
-  //           name: course.courseTitle,
-  //           images: [course.courseThumbnail],
-  //         },
-  //         unit_amount: course.coursePrice * 100, // Amount in paise (lowest denomination)
-  //       },
-  //       quantity: 1,
-  //     },
-  //   ],
-  //   mode: "payment",
-  //   success_url: `http://localhost:5173/course-progress/${courseId}`, // once payment successful redirect to course progress page
-  //   cancel_url: `http://localhost:5173/course-detail/${courseId}`,
-  //   metadata: {
-  //     courseId: courseId,
-  //     userId: userId,
-  //   },
-  //   shipping_address_collection: {
-  //     allowed_countries: ["IN"], // Optionally restrict allowed countries
-  //   },
-  // });
-
-  // if (!session.url) {
-  //   return res
-  //     .status(400)
-  //     .json({ success: false, message: "Error while creating session" });
-  // }
-
-
-
-  //   return res.status(200).json({
-  //     success: true,
-  //     url: session.url, // Return the Stripe checkout URL
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  // }
 };
 
 export const stripeWebhook = async (req, res) => {
